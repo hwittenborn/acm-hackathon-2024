f897dd9526dfe30a64de50c87cd00fe78718a3cf
Ian-Bohanan <icb0003@uah.edu>
2024-03-23 17:04:48 -0500

    removed openCamera from checking requirements

--- openCamera.py
+++ openCamera.py
@@ -4,18 +4,4 @@ import sys
 
 print("Booting up openCamera")
 
-def checkRequirements():
-    required_libraries = [
-        "opencv-python",
-        "requests"
-    ]
-    for lib in required_libraries:
-        try:
-            subprocess.check_call([sys.executable, '-m', 'pip', 'install', lib])
-        except subprocess.CalledProcessError: 
-            print("ERROR installing libraries")
-   
-
-print("Checking requirements")
-checkRequirements()
 request.getCurFrame()
