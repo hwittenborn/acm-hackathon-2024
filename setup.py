40a28401743158ee902172056fdab86c2eb84399
Ian-Bohanan <icb0003@uah.edu>
2024-03-23 16:36:22 -0500

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+def checkRequirements():
+    required_libraries = [
+        "opencv-python",
+        "requests"
+    ]
+    for lib in required_libraries:
+        try:
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', lib])
+        except subprocess.CalledProcessError: 
+            print("ERROR installing libraries")
+
+checkRequirements()
