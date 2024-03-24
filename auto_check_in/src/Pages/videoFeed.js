3b39f478f5df15997509cee39244e031510b0235
Ian-Bohanan <icb0003@uah.edu>
2024-03-23 22:06:01 -0500

    Add basic RSVPGuestContainer to feed

--- auto_check_in/src/Pages/videoFeed.js
+++ auto_check_in/src/Pages/videoFeed.js
@@ -1,9 +1,23 @@
 import "./feed.css"
+import appleBowlImage from '../RSVPPics/Ian Bohanan.jpg';
+
+function RSVPGuest() {
+    return (
+      <div className="RSVPGuestContainer">
+        <div style={{ margin: '5%' }}>
+            <img className="GuestIcon" src={appleBowlImage}/>
+            <p>Ian Bohanan</p>
+        </div>
+            
+      </div>
+    );
+}
 
 export function VideoFeed(){
     return(
         <>
         <h1>This is the video feed</h1>
+        <RSVPGuest/>
         </>
     )
 }
