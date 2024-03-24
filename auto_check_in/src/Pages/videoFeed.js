258b187fa4156c208dd9346aee5f2309f0abccf5
Ian-Bohanan <icb0003@uah.edu>
2024-03-23 22:56:48 -0500

    Load images dynamically in "images" array

--- auto_check_in/src/Pages/videoFeed.js
+++ auto_check_in/src/Pages/videoFeed.js
@@ -1,14 +1,16 @@
 import "./feed.css"
 import TestIcon from '../RSVPPics/Ian Bohanan.jpg';
+import React, { useEffect, useState } from 'react';
 
-function RSVPGuest() {
+function RSVPGuest({image,name}) {
+    const isCheckedIn = false;
     return (
       <div className="RSVPGuestContainer">
         <div style={{ margin: '5%', display:'flex', }}>
-            <img className="GuestIcon" src={TestIcon}/>
+            <img className="GuestIcon" src={image}/>
             <div>
-                <p>Ian Bohanan</p>
-                <p>Checked in: 5:00PM</p>
+                <p>{name}</p>
+                {isCheckedIn ? <p>Checked in 5:00PM</p> : <p>Not checked in</p>}
             </div>
         </div>
       </div>
@@ -31,7 +33,31 @@ function UnknownGuest(){
       );
 }
 
+
+
 export function VideoFeed(){
+
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        // Dynamically require images
+        const context = require.context('../RSVPPics/', false, /\.(jpg|jpeg|png)$/);
+        const imagePaths = context.keys().map(context);
+    
+        setImages(imagePaths);
+      }, []);
+
+      const persons = [
+        {
+          image: images[0],
+          name: 'Ian Bohanan',
+        },
+        {
+          image: images[1],
+          name: 'John Doe'
+        }
+      ];
+
     return(
         <>
         <h1>This is the video feed</h1>
@@ -40,8 +66,9 @@ export function VideoFeed(){
 
             <div className="KnownGuests">
                 <h2>Known Guests</h2>
-                <RSVPGuest/>
-                <RSVPGuest/>
+                {persons.map((person, index) => (
+                    <RSVPGuest key={index} image={person.image} name={person.name} />
+                ))}
             </div>
 
             <div className="UnknownGuests">
